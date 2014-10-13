be0b4b3fe78342ce97ba22c5f29b6447ba78e7e6
Armin Graf <arminhammer@gmail.com>
2014-10-13 14:15:38 -0400

    Added basic download capability.

--- cmds/get.js
+++ cmds/get.js
@@ -5,12 +5,37 @@
 
 module.exports = function(program) {
 
+	var request = require('request');
+	var fs = require('fs');
+	var os = require('os');
+	var url = require('url');
+
 	program
 		.command('get')
 		.version('0.0.1')
 		.description('Download a web page as a single-file archive')
-		.action(function(url){
-			console.log("Getting page %s", url);
+		.action(function(urlArg){
+
+			var urlObj = url.parse(urlArg);
+
+			// Determine the filename of the url.  If it is not in the path, assume index.html.
+			var filename = urlObj.pathname.slice(urlObj.pathname.lastIndexOf('/')+1);
+			if(filename == "") {
+				filename = "index.html";
+			}
+
+			console.log(filename);
+
+
+			console.log("Temp dir: " + os.tmpdir());
+			var tempLocation = os.tmpdir() + "/" + filename + ".part";
+			request(urlArg).pipe(fs.createWriteStream(tempLocation));
+			console.log("Getting page %s", urlArg);
+			fs.createReadStream(tempLocation).pipe(fs.createWriteStream(filename));
+			fs.unlink(tempLocation, function() {
+				console.log("Deleted %s", tempLocation);
+			})
+
 		});
 	
 };
