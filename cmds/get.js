da381d1d491676865898988f74bc9d5d1855bf67
Armin Graf <arminhammer@gmail.com>
2014-10-13 17:03:46 -0400

    Data URI creation works, need to get it to call those functions before writing to file.

--- cmds/get.js
+++ cmds/get.js
@@ -6,9 +6,12 @@
 module.exports = function(program) {
 
 	var request = require('request');
+	var cheerio = require('cheerio');
 	var fs = require('fs');
 	var os = require('os');
 	var url = require('url');
+	var mime = require('mime');
+	var util = require('util');
 
 	program
 		.command('get')
@@ -29,13 +32,64 @@ module.exports = function(program) {
 
 			console.log("Temp dir: " + os.tmpdir());
 			var tempLocation = os.tmpdir() + "/" + filename + ".part";
-			request(urlArg).pipe(fs.createWriteStream(tempLocation));
-			console.log("Getting page %s", urlArg);
-			fs.createReadStream(tempLocation).pipe(fs.createWriteStream(filename));
-			fs.unlink(tempLocation, function() {
-				console.log("Deleted %s", tempLocation);
-			})
+
+			var html = "";
+
+			console.log("Length: " + html.length);
+			embedImages(urlArg, html, function() {
+				console.log("Length: " + html.length);
+			});
+
+			/*
+			fs.writeFile(filename, html, function (err) {
+				if (err) throw err;
+				console.log('It\'s saved!');
+			});
+			*/
+
+			//.pipe(fs.createWriteStream(tempLocation));
+
+
+			/*
+			 console.log("Getting page %s", urlArg);
+			 fs.createReadStream(tempLocation).pipe(fs.createWriteStream(filename));
+			 fs.unlink(tempLocation, function() {
+			 console.log("Deleted %s", tempLocation);
+			 })
+			 */
 
 		});
-	
+
+
+	function embedImages(pageURL, html, callback) {
+
+		(function() {
+			request({ uri: pageURL }, function (error, response, body) {
+
+				var $ = cheerio.load(body);
+
+				$('img').each(function (i, elem) {
+					var img = $(this);
+					var imageURL = img.attr('src');
+					console.log(imageURL);
+
+
+					request({ uri: img.attr('src') }, function (imageError, imageResponse, imageBody) {
+						var imageData = new Buffer(imageBody).toString('base64');
+						console.log("Type: " + mime.lookup(imageURL));
+						console.log("Size: " + imageData.length);
+						var dataUri = util.format("data:%s;base64,%s", mime.lookup(imageURL), imageData);
+						img.attr('src');
+					});
+
+				});
+
+				html = $.html();
+
+			});
+		})();
+
+		callback();
+	}
+
 };
