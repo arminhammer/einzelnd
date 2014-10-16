061f3370d0f976a7ce0ad401ff8f67f7824c375e
arminhammer <arminhammer@gmail.com>
2014-10-15 23:10:22 -0400

    Control flow works, but the buffers being generated for the data Uris are all the same.  Needs to be fixed.

--- cmds/get.js
+++ cmds/get.js
@@ -5,103 +5,125 @@
 
 module.exports = function(program) {
 
-	var cheerio = require('cheerio');
-	var os = require('os');
-	var url = require('url');
-	var mime = require('mime');
-	var util = require('util');
-	var http = require('http');
-	var Promise = require('bluebird');
+    var cheerio = require('cheerio');
+    var os = require('os');
+    var url = require('url');
+    var mime = require('mime');
+    var util = require('util');
+    var http = require('http');
+    var Promise = require('bluebird');
 
-	var fs = Promise.promisifyAll(require('fs'), { suffix: "PS" });
-	var request = Promise.promisifyAll(require('request'), { suffix: "PS" });
+    var fs = Promise.promisifyAll(require('fs'), { suffix: "PS" });
+    var request = Promise.promisifyAll(require('request'), { suffix: "PS" });
 
-	program
-		.command('get')
-		.version('0.0.1')
-		.description('Download a web page as a single-file archive')
-		.action(function(urlArg) {
+    program
+        .command('get')
+        .version('0.0.1')
+        .description('Download a web page as a single-file archive')
+        .action(function(urlArg) {
 
-			var urlObj = url.parse(urlArg);
+            var urlObj = url.parse(urlArg);
 
-			// Determine the filename of the url.  If it is not in the path, assume index.html.
-			var filename = urlObj.pathname.slice(urlObj.pathname.lastIndexOf('/') + 1);
-			if (filename == "") {
-				filename = "index.html";
-			}
+            // Determine the filename of the url.  If it is not in the path, assume index.html.
+            var filename = urlObj.pathname.slice(urlObj.pathname.lastIndexOf('/') + 1);
+            if (filename == "") {
+                filename = "index.html";
+            }
 
-			console.log(filename);
+            console.log(filename);
 
-			var scope = {
-				html: ""
-			};
+            // Maintain the state of the file to ultimately be written
+            var scope = {
+                html: ""
+            };
 
-			request.getPS(urlArg)
-				.then(function (data) {
-					//scope.html = data;
-					fs.writeFileSync("scope.html", scope.html);
-					return getImageArray(scope.html);
-				})
-				.map(function(link) {
-					console.log("Link %s", link);
-					fs.writeFileSync("scope1.html", scope.html);
-					return request.getPS(link)
-						.then(function(imageData) {
-							buildDataUri(link, imageData, scope);
-						});
-				})
-				.then(function(response) {
-					console.log("Response: %s, link: %s", response);
-				})
-				.then(function (response) {
-					fs.writeFilePS(filename, scope.html);
-				})
-				.then(function () {
-					console.log("Finished writing %s", filename);
-				});
-		});
+            /**
+             * Start off the chain of events by getting a copy of the web page
+             */
+            request.getPS(urlArg)
+                // Then get an array of all of the image URLs
+                .then(function (data) {
+                    scope.html = data;
+                    return getImageArray(scope.html);
+                })
+                //For each image URL, download it, and replace the URL in the page with the dataUri
+                .map(function(link) {
+                    console.log("Link %s", link);
+                    return request.getPS(link)
+                        .then(function(imageData) {
+                            //console.log("Image Data: " + imageData.toString());
+                            buildDataUri(link, imageData, scope);
+                        });
+                })
+                //Then write the file to disk
+                .then(function (response) {
+                    fs.writeFilePS(filename, scope.html);
+                })
+                // Notify the user
+                .then(function () {
+                    console.log("Finished writing %s", filename);
+                });
+        });
 
-	function getImageArray(body) {
+    /**
+     * Get an array of all of the urls in img.src's on the page
+     * @param body
+     * @returns {Array}
+     */
+    function getImageArray(body) {
 
-		var $ = cheerio.load(body.toString());
+        var $ = cheerio.load(body.toString());
 
-		console.log("Loading $");
+        console.log("Loading $");
 
-		var linkArray = [];
+        var linkArray = [];
 
-		$('img').each(function (i, elem) {
+        $('img').each(function (i, elem) {
 
-			console.log("Returning");
+            console.log("Returning");
 
-			var img = $(this);
-			var imageURL = img.attr('src');
-			linkArray.push(imageURL);
+            var img = $(this);
+            var imageURL = img.attr('src');
+            linkArray.push(imageURL);
 
-		});
+        });
 
-		console.log(linkArray);
-		return linkArray;
+        console.log(linkArray);
+        return linkArray;
 
-	}
+    }
 
-	function buildDataUri(link, imgData, scope) {
+    /**
+     * Build the dataUri for each image URL, and replace the URL with the dataUri
+     *
+     * @param link
+     * @param imgData
+     * @param scope
+     * @returns {string}
+     *
+     */
+    function buildDataUri(link, imgData, scope) {
 
-		var $ = cheerio.load(scope.html.toString());
+        var $ = cheerio.load(scope.html.toString());
 
-		console.log("Link: %s", link);
-		var img = $('img').attr('src', link);
-		console.log("Tag: " + img);
+        console.log("Link: %s", link);
+        //console.log("Status code: %s", imgData.statusCode);
+        var img = $('img').filter(function() {
+            return $(this).attr('src') === link;
+        });
+        console.log("Tag: " + img);
 
+        var dataString = new Buffer(imgData.toString(), 'binary').toString('base64');
+        console.log("Type: " + mime.lookup(link));
+        console.log("Size: " + dataString.length);
+        console.log("Datastring: " + dataString.substring(0, 100));
+        var dataUri = util.format("data:%s;base64,%s", mime.lookup(link), dataString);
+        img.attr('src', dataUri);
+        //console.log("New Tag: " + img);
 
-		var dataString = new Buffer(imgData).toString('base64');
-		console.log("Type: " + mime.lookup(link));
-		console.log("Size: " + dataString.length);
-		var dataUri = util.format("data:%s;base64,%s", mime.lookup(link), dataString);
-		img.attr('src', dataUri);
+        scope.html = $.html();
+        return scope.html;
 
-		scope.html = $.html();
-		return;
-
-	}
+    }
 
 };
